1a9fb69eeb446dbc66f1cae1b686fa15704eb05e
Ninesquared81 <96393839+Ninesquared81@users.noreply.github.com>
2023-09-02 00:45:38 +0100

    Add main.c

--- src/interpreter.c
+++ src/interpreter.c
@@ -1,6 +1,6 @@
 #include "interpreter.h"
 
 enum interpret_result interpret(void) {
-
+    return INTERPRET_OK;
 }
 
---
+++ src/main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+#include "interpreter.h"
+
+
+int main(void) {
+    printf("%d\n", interpret());
+    return 0;
+}
