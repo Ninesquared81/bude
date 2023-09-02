78575dd781ab7c9cd3b00390327c4f3c486e878b
Ninesquared81 <96393839+Ninesquared81@users.noreply.github.com>
2023-09-02 03:03:21 +0100

    Add temporary test code in main.c

--- src/main.c
+++ src/main.c
@@ -1,9 +1,17 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 #include "interpreter.h"
+#include "ir.h"
 
 
 int main(void) {
-    printf("%d\n", interpret());
+    struct ir_block *block = allocate_block(0);
+    write_immediate(block, OP_PUSH, 1);
+    write_immediate(block, OP_PUSH, 2);
+    write_simple(block, OP_ADD);
+    write_simple(block, OP_PRINT);
+    interpret(block);
+    free_block(block);
     return 0;
 }
