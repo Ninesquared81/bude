7b25208d10c8f57f209121f08a1f6ee1966e0572
Ninesquared81 <96393839+Ninesquared81@users.noreply.github.com>
2023-09-02 03:01:43 +0100

    Use disassembler in interpreter (temporary)

--- src/interpreter.c
+++ src/interpreter.c
@@ -1,6 +1,9 @@
 #include "interpreter.h"
+#include "disassembler.h"
+#include "ir.h"
 
-enum interpret_result interpret(void) {
+enum interpret_result interpret(struct ir_block *block) {
+    disassemble_block(block);
     return INTERPRET_OK;
 }
 
--- src/interpreter.h
+++ src/interpreter.h
@@ -1,11 +1,13 @@
 #ifndef INTERPRETER_H
 #define INTERPRETER_H
 
+#include "ir.h"
+
 enum interpret_result {
     INTERPRET_OK,
     INTERPRET_ERROR,
 };
 
-enum interpret_result interpret(void);
+enum interpret_result interpret(struct ir_block *block);
 
 #endif
