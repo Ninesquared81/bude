f1b122204d743a0d3714de31986f0c1e1d02c331
Ninesquared81 <96393839+Ninesquared81@users.noreply.github.com>
2023-09-05 21:52:09 +0100

    Read source code from file instead of hardcoding

--- src/main.c
+++ src/main.c
@@ -6,8 +6,47 @@
 #include "ir.h"
 
 
-int main(void) {
-    const char *code = "if 1 then 42 print else 54 print end 5 print";
+#define INPUT_BUFFER_SIZE 4 * 1024 * 1024
+
+static void print_usage(FILE *file, const char *name) {
+    fprintf(file, "%s <file>\n", name);
+}
+
+static void parse_args(int argc, char *argv[], char *inbuf) {
+    const char *name = argv[0];
+    if (argc == 1) {
+        fprintf(stderr, "Error: missing positional argument.\nUsage: ");
+        print_usage(stderr, name);
+        exit(1);
+    }
+    if (argc > 2) {
+        fprintf(stderr, "Warning: excess arguments discarded.\nUsage: ");
+    }
+    FILE *file = fopen(argv[1], "r");
+    if (file == NULL) {
+        perror("Could not open file");
+        exit(1);
+    }
+    fread(inbuf, sizeof *inbuf, INPUT_BUFFER_SIZE, file);
+    if (ferror(file)) {
+        perror("Error reading file");
+        exit(1);
+    }
+    if (!feof(file)) {
+        perror("File too large");
+        exit(1);
+    }
+    fclose(file);
+}
+
+int main(int argc, char *argv[]) {
+    char *inbuf = calloc(INPUT_BUFFER_SIZE, sizeof *inbuf);
+    if (inbuf == NULL) {
+        fprintf(stderr, "calloc failed!\n");
+        exit(1);
+    }
+    parse_args(argc, argv, inbuf);
+
     struct ir_block block;
     init_block(&block);
     /*
@@ -16,8 +55,11 @@ int main(void) {
     write_simple(block, OP_ADD);
     write_simple(block, OP_PRINT);
     */
-    compile(code, &block);
+    compile(inbuf, &block);
+    free(inbuf);
+
     interpret(&block);
     free_block(&block);
+
     return 0;
 }
