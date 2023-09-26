50351fd46c6371181bd68226bef3cbb1c8bc5459
Ninesquared81 <96393839+Ninesquared81@users.noreply.github.com>
2023-09-26 02:41:29 +0100

    Support `OP_SUB` in generator

--- src/generator.c
+++ src/generator.c
@@ -8,6 +8,14 @@
 
 
 enum generate_result generate(struct ir_block *block, struct asm_block *assembly) {
+#define BIN_OP(OP)                                               \
+    do {                                                         \
+        asm_write(assembly, "\tpop\trbx\t\t; RHS.\n");           \
+        asm_write(assembly, "\tpop\trax\t\t; LHS.\n");           \
+        asm_write(assembly, "\t"OP"\trax, rbx\n");               \
+        asm_write(assembly, "\tpush\trax\n");                    \
+    } while (0)
+
     asm_start_asm(assembly);
     asm_start_code(assembly, "start");
     for (int ip = 0; ip < block->count; ++ip) {
@@ -26,15 +34,16 @@ enum generate_result generate(struct ir_block *block, struct asm_block *assembly
         }
         case OP_ADD:
             asm_write(assembly, "  ;; === OP_ADD ===\n");
-            asm_write(assembly, "\tpop\trbx\t\t; RHS.\n");
-            asm_write(assembly, "\tpop\trax\t\t; LHS.\n");
-            asm_write(assembly, "\tadd\trax, rbx\n");
-            asm_write(assembly, "\tpush\trax\n");
+            BIN_OP("add");
             break;
         case OP_EXIT:
             asm_write(assembly, "  ;; === OP_EXIT ===\n");
             asm_write(assembly, "\tpop\trcx\t\t; Exit code.\n");
             asm_write(assembly, "\tcall\t[ExitProcess]\n");
+            break;
+        case OP_SUB:
+            asm_write(assembly, "  ;; === OP_SUB ===\n");
+            BIN_OP("sub");
             break;
         default:
             assert(0 && "Not implemented you silly goose!");
@@ -42,4 +51,5 @@ enum generate_result generate(struct ir_block *block, struct asm_block *assembly
     }
     asm_end_code(assembly);
     return GENERATE_OK;
+#undef BIN_OP
 }
