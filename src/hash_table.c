bdaeb774adccdeabf7e126be9de4810cbd6e8d87
Ninesquared81 <96393839+Ninesquared81@users.noreply.github.com>
2023-10-08 22:17:10 +0100

    Rename `hash_string()` to `hash_sv()` in hash_table.c

--- src/hash_table.c
+++ src/hash_table.c
@@ -1,6 +1,6 @@
 #include "hash_table.h"
 
-uint32_t hash_string(struct string_view *key) {
+uint32_t hash_sv(struct string_view *key) {
     uint32_t hash = 2166136261u;
     for (size_t i = 0; i < key->length; ++i) {
         hash ^= *key->start[i];
