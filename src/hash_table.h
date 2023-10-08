112eb2eb6dfbd8eb50208955e0e299f9f35c2130
Ninesquared81 <96393839+Ninesquared81@users.noreply.github.com>
2023-10-09 00:03:09 +0100

    Use separate chaining

--- src/hash_table.h
+++ src/hash_table.h
@@ -1,18 +1,27 @@
 #ifndef HASH_TABLE_H
 #define HASH_TABLE_H
 
+#include <stdbool.h>
 #include <stdint.h>
 
 struct hash_table {
     size_t capacity;
     size_t count;
-    struct table_element *elements;
+    struct table_bucket *buckets;
+    struct region *element_region;
 };
 
 struct table_element {
+    // We use separate chaining.
     struct table_element *next;
-    struct string_view *sv;
+    struct string_view sv;
 };
+
+struct table_bucket {
+    struct table_element head;
+    uint32_t hash;
+    bool is_occupied;
+}
 
 uint32_t hash_sv(struct string_view *key);
 
