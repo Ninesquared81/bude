bf86abde5533f4bcf1863332a367fccde9aee77f
Ninesquared81 <96393839+Ninesquared81@users.noreply.github.com>
2023-11-06 04:51:26 +0000

    Add space between rpeorted loctation and message in error messages

--- src/location.c
+++ src/location.c
@@ -3,5 +3,5 @@
 #include "location.h"
 
 void report_location(const char *restrict filename, const struct location *location) {
-    fprintf(stderr, "%s:%zu:%zu:", filename, location->line, location->column);
+    fprintf(stderr, "%s:%zu:%zu: ", filename, location->line, location->column);
 }
