8fe19346073bcecf360b873f70a4e0e8ba562fd3
Ninesquared81 <96393839+Ninesquared81@users.noreply.github.com>
2024-03-22 03:27:28 +0000

    Introduce `pair32` to type punning
    This allows us to pack two 32-bit integers into a 64-bit integer,
    letting us reuse the basic 64-bit stack.

--- src/type_punning.h
+++ src/type_punning.h
@@ -8,6 +8,9 @@ union pun16 {uint16_t u16; int16_t s16;};
 union pun32 {uint32_t u32; int32_t s32;};
 union pun64 {uint64_t u64; int64_t s64;};
 
+struct pair32 {uint32t a, b;};
+union pun_pair32 {struct pair32 pair32; uint64_t u64;};
+
 #define u8_to_s8(value) ((union pun8) {.u8 = value}).s8
 #define s8_to_u8(value) ((union pun8) {.s8 = value}).u8
 
@@ -20,5 +23,7 @@ union pun64 {uint64_t u64; int64_t s64;};
 #define u64_to_s64(value) ((union pun64) {.u64 = value}).s64
 #define s64_to_u64(value) ((union pun64) {.s64 = value}).u64
 
+#define pair32_to_u64(value) ((union pun_tup32) {.pair32 = value}).u64
+#define u64_to_pair32(value) ((union pun_tup32) {.u64 = value}).pair32
 
 #endif
