9b01d032bb90caa73c787c4e410081e54b30223a
Ninesquared81 <96393839+Ninesquared81@users.noreply.github.com>
2024-04-04 22:49:45 +0100

    Introduce `array_eq()` to memory module

--- src/memory.c
+++ src/memory.c
@@ -1,3 +1,4 @@
+#include <assert.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -28,3 +29,11 @@ void *reallocate_array(void *array, size_t old_count, size_t new_count, size_t s
     return new;
 }
 
+bool array_eq(size_t a_count, const void *a, size_t b_count, const void *b, size_t size) {
+    if (a_count != b_count) return false;
+    if (a_count == 0) return true;
+    assert(a != NULL);
+    assert(b != NULL);
+    assert(size > 0);
+    return memcmp(a, b, a_count * size) == 0;
+}
--- src/memory.h
+++ src/memory.h
@@ -1,6 +1,7 @@
 #ifndef MEMORY_H
 #define MEMORY_H
 
+#include <stdbool.h>
 #include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -15,5 +16,7 @@
 void *allocate_array(size_t count, size_t size);
 void free_array(void *array, [[maybe_unused]] size_t count, [[maybe_unused]] size_t size);
 void *reallocate_array(void *array, size_t old_count, size_t new_count, size_t size);
+
+bool array_eq(size_t a_count, const void *a, size_t b_count, const void *b, size_t size);
 
 #endif
