428fee59fb6a569f98f4c219676c01377effc6c4
Ninesquared81 <96393839+Ninesquared81@users.noreply.github.com>
2024-05-09 04:10:05 +0100

    Add placeholder code for analyser
    This just displays the bytecode in the human-readable format.

--- tool/analyser.py
+++ tool/analyser.py
@@ -1,8 +1,23 @@
 """This module analyses the bytecode produced by the Bude compiler."""
 
+import argparse
+
+import reader
+
 
 def main() -> None:
-    pass
+    arg_parser = argparse.ArgumentParser(description="Analyse a BudeBWF file")
+    arg_parser.add_argument("filename", help="the file to analyse")
+    args = arg_parser.parse_args()
+    try:
+        strings, functions = reader.read_bytecode(args.filename)
+    except reader.ParseError as e:
+        print(e)
+        exit(1)
+    except IOError as e:
+        print(e)
+        exit(1)
+    reader.display_bytecode(strings, functions)
 
 
 if __name__ == "__main__":
