544d233195df74ae531b03c95e887576f6d592a1
Ninesquared81 <96393839+Ninesquared81@users.noreply.github.com>
2024-05-11 04:05:23 +0100

    Add separate functions for plotting different subplots

--- tool/analyser.py
+++ tool/analyser.py
@@ -11,23 +11,36 @@ import ir
 import reader
 
 
+def plot_bar(fig: plt.Figure, counts: np.ndarray, opcode_names: list[str]) -> None:
+    """Plot a bar chart to show the frequency of each IR instruction."""
+    BAR_THICKNESS = 10.
+    ax = plt.subplot(2, 1, 1)
+    ax.tick_params(axis="x", rotation=90, labelsize="xx-small")
+    bar_xs = [op * (BAR_THICKNESS + 1) for op in ir.Opcode]
+    ax.bar(bar_xs, counts, width=BAR_THICKNESS, tick_label=opcode_names)
+
+
+def plot_pie(fig: plt.Figure, counts: np.ndarray, opcode_names: list[str]) -> None:
+    """Plot a pie chart to show the frequency of each IR instruction."""
+    ax = plt.subplot(2, 1, 2)
+    pie_xs, pie_labels = zip(
+        *((count, name) for count, name
+          in zip(counts, opcode_names) if count > 0)
+    )
+    ax.pie(pie_xs, labels=pie_labels)
+
+
 def analyse_bytecode(functions: list[bytes]) -> None:
+    """Analyse the frequency of each IR instruction."""
     counts = np.zeros(len(ir.Opcode), dtype=int)
     opcode_names = [op.name for op in ir.Opcode]
     for i, func in enumerate(functions):
         print(f"func_{i}:", *ir.Block(func), sep="\n\t")
         for instruction in ir.Block(func):
             counts[instruction.op] += 1
-    BAR_THICKNESS = 10.
-    plt.figure(figsize=(16., 6.4))
-    plt.xticks(rotation=90, fontsize="xx-small")
-    bar_xs = [op * (BAR_THICKNESS + 1) for op in ir.Opcode]
-    plt.bar(bar_xs, counts, width=BAR_THICKNESS, tick_label=opcode_names)
-    pie_xs, pie_labels = zip(
-        *((count, name) for count, name
-          in zip(counts, opcode_names) if count > 0)
-    )
-    plt.pie(pie_xs, labels=pie_labels)
+    fig = plt.figure(figsize=(16., 10.))
+    plot_bar(fig, counts, opcode_names)
+    plot_pie(fig, counts, opcode_names)
     plt.show()
 
 
