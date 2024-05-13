d2ed3583f29160a6bc0e9deeb391e9d91327f48a
Ninesquared81 <96393839+Ninesquared81@users.noreply.github.com>
2024-05-14 00:54:37 +0100

    Add `exmgr.py` script to manage examples
    This is (will be) a utility script for working on every file in the
    examples directory at once. The intention is to support various
    operations, such as IR dumping, asm generation and BudeBWF
    generation (useful for analyser.py).

---
+++ tool/exmgr.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+"""Example manager -- build, run, etc. all the Bude examples in the example directory."""
+
+import pathlib
+import subprocess
+
+
+example_dir = pathlib.Path("../examples/").resolve()
+bude_sources = example_dir.glob("*.bude")
+bude_exe = pathlib.Path("../bin/bude.exe").resolve()
+
+error_level = 0
+for filename in bude_sources:
+    proc = subprocess.run([str(bude_exe), str(filename), "-d"],
+                          capture_output=True)
+    exit_status = proc.returncode
+    error_level += exit_status
+    if exit_status == 0:
+        print(f"File {filename.name!r} compiled successfully:")
+        # print(proc.stdout.decode())
+    else:
+        print(f"File {filename.name!r} failed to compile:",
+              proc.stderr.decode(), sep="\n")
+
+if error_level == 0:
+    print("Whole suite compiled successfully!")
+else:
+    print("Suite compiled with errors.")
+    exit(1)
