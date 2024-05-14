e76eeab1a224725c10324c5baf84ed5ecb2a001f
Ninesquared81 <96393839+Ninesquared81@users.noreply.github.com>
2024-05-14 02:21:26 +0100

    Add `-v`/`--verbose` option to exmgr

--- tool/exmgr.py
+++ tool/exmgr.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """Example manager -- build, run, etc. all the Bude examples in the example directory."""
 
+import argparse
 import pathlib
 import subprocess
 
@@ -10,7 +11,7 @@ BUDE_SOURCES = EXAMPLE_DIR.glob("*.bude")
 BUDE_EXE = pathlib.Path("../bin/bude.exe").resolve()
 
 
-def compile_dump_ir():
+def compile_dump_ir(args: argparse.Namespace):
     error_level = 0
     for filename in BUDE_SOURCES:
         proc = subprocess.run([str(BUDE_EXE), str(filename), "-d"],
@@ -19,7 +20,8 @@ def compile_dump_ir():
         error_level += abs(exit_status)
         if exit_status == 0:
             print(f"File {filename.name!r} compiled successfully ({exit_status}):")
-            # print(proc.stdout.decode())
+            if args.verbose:
+                print(proc.stdout.decode())
         else:
             print(f"File {filename.name!r} failed to compile ({exit_status}):",
                   proc.stderr.decode(), sep="\n")
@@ -32,7 +34,10 @@ def compile_dump_ir():
 
 
 def main():
-    compile_dump_ir()
+    arg_parser = argparse.ArgumentParser()
+    arg_parser.add_argument("-v", "--verbose", action="store_true", help="verbose mode")
+    args = arg_parser.parse_args()
+    compile_dump_ir(args)
 
 if __name__ == "__main__":
     main()
