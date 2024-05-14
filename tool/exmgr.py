87a21a47061ac4f8c3b2d1c8baf4b07188e29068
Ninesquared81 <96393839+Ninesquared81@users.noreply.github.com>
2024-05-14 19:49:02 +0100

    Replace `-q` option with `--message-level` in exmgr
    The message level is an integer in the range [-2, 2] which controls
    the amount of diagnostic messages emitted by exmgr. The lower the
    number, the fewer messages are emitted.

--- tool/exmgr.py
+++ tool/exmgr.py
@@ -11,6 +11,13 @@ OUTPUT_DIR = pathlib.Path("./output").resolve()
 BUDE_SOURCES = EXAMPLE_DIR.glob("*.bude")
 BUDE_EXE = pathlib.Path("../bin/bude.exe").resolve()
 
+log_level = 0
+
+
+def log_with_level(target_level, *print_args, **print_kwargs):
+    if log_level >= target_level:
+        print(*print_args, **print_kwargs)
+
 
 def compile_handler(args: argparse.Namespace):
     error_level = 0
@@ -22,31 +29,33 @@ def compile_handler(args: argparse.Namespace):
         if args.dump:
             bude_args.append("-d")
         if not args.no_write:
-            new_name = ".".join(name, "bbwf")
+            new_name = ".".join((name, "bbwf"))
             output_path = OUTPUT_DIR / new_name
             bude_args.extend(["-o", str(output_path)])
         proc = subprocess.run([str(BUDE_EXE), str(filename), *bude_args],
                               capture_output=True)
-        print(*proc.args)
+        log_with_level(2, *proc.args)
         exit_status = proc.returncode
         error_level += abs(exit_status)
         if exit_status == 0:
-            print(f"File {filename.name!r} compiled successfully ({exit_status}):")
-            if not args.quiet:
-                print(proc.stdout.decode())
+            log_with_level(-1, f"File {filename.name!r} compiled successfully ({exit_status}):")
+            log_with_level(1, proc.stdout.decode())
         else:
-            print(f"File {filename.name!r} failed to compile ({exit_status}):",
-                  proc.stderr.decode(), sep="\n")
+            log_with_level(-1, f"File {filename.name!r} failed to compile ({exit_status}):")
+            log_with_level(0, proc.stderr.decode())
 
     if error_level == 0:
-        print("Whole suite compiled successfully!")
+        log_with_level(-2, "Whole suite compiled successfully!")
     else:
-        print("Suite compiled with errors.")
+        log_with_level(-2, "Suite compiled with errors.")
         exit(1)
 
 
 def main():
     arg_parser = argparse.ArgumentParser(description=__doc__)
+    arg_parser.add_argument("--message-level", action="store",
+                            type=int, choices=range(-2, 3), default=0,
+                            help="level of diagnostic messages to emit (lower -> fewer messages)")
     subparsers = arg_parser.add_subparsers(title="subcommands", required=True)
     compile_parser = subparsers.add_parser(
         "compile", help="compile files to word-oriented IR code"
@@ -56,15 +65,13 @@ def main():
         help="dump IR code to stdout"
     )
     compile_parser.add_argument(
-        "-q", "--quiet", action="store_true",
-        help="do not emit stdout from Bude process"
-    )
-    compile_parser.add_argument(
         "-n", "--no-write", action="store_true",
         help="compile to IR but don't save to BudeBWF file"
     )
     compile_parser.set_defaults(handler=compile_handler)
     args = arg_parser.parse_args()
+    global log_level
+    log_level = args.message_level
     args.handler(args)
 
 
