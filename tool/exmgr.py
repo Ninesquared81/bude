3275349e60b72205769164780112c92ea19125c1
Ninesquared81 <96393839+Ninesquared81@users.noreply.github.com>
2024-05-14 01:05:23 +0100

    Add `main()` function to exmgr

--- tool/exmgr.py
+++ tool/exmgr.py
@@ -5,25 +5,30 @@ import pathlib
 import subprocess
 
 
-example_dir = pathlib.Path("../examples/").resolve()
-bude_sources = example_dir.glob("*.bude")
-bude_exe = pathlib.Path("../bin/bude.exe").resolve()
+def main():
+    example_dir = pathlib.Path("../examples/").resolve()
+    bude_sources = example_dir.glob("*.bude")
+    bude_exe = pathlib.Path("../bin/bude.exe").resolve()
 
-error_level = 0
-for filename in bude_sources:
-    proc = subprocess.run([str(bude_exe), str(filename), "-d"],
-                          capture_output=True)
-    exit_status = proc.returncode
-    error_level += abs(exit_status)
-    if exit_status == 0:
-        print(f"File {filename.name!r} compiled successfully:")
-        # print(proc.stdout.decode())
+    error_level = 0
+    for filename in bude_sources:
+        proc = subprocess.run([str(bude_exe), str(filename), "-d"],
+                              capture_output=True)
+        exit_status = proc.returncode
+        error_level += abs(exit_status)
+        if exit_status == 0:
+            print(f"File {filename.name!r} compiled successfully:")
+            # print(proc.stdout.decode())
+        else:
+            print(f"File {filename.name!r} failed to compile:",
+                  proc.stderr.decode(), sep="\n")
+
+    if error_level == 0:
+        print("Whole suite compiled successfully!")
     else:
-        print(f"File {filename.name!r} failed to compile:",
-              proc.stderr.decode(), sep="\n")
+        print("Suite compiled with errors.")
+        exit(1)
 
-if error_level == 0:
-    print("Whole suite compiled successfully!")
-else:
-    print("Suite compiled with errors.")
-    exit(1)
+
+if __name__ == "__main__":
+    main()
