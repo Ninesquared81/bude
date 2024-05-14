b18563bc1473af01c285c933e69ca5649d8b3b5f
Ninesquared81 <96393839+Ninesquared81@users.noreply.github.com>
2024-05-14 17:26:37 +0100

    Move current functionality of exmgr to `compile` subcommand

--- tool/exmgr.py
+++ tool/exmgr.py
@@ -11,7 +11,7 @@ BUDE_SOURCES = EXAMPLE_DIR.glob("*.bude")
 BUDE_EXE = pathlib.Path("../bin/bude.exe").resolve()
 
 
-def compile_dump_ir(args: argparse.Namespace):
+def compile_handler(args: argparse.Namespace):
     error_level = 0
     for filename in BUDE_SOURCES:
         proc = subprocess.run([str(BUDE_EXE), str(filename), "-d"],
@@ -20,7 +20,7 @@ def compile_dump_ir(args: argparse.Namespace):
         error_level += abs(exit_status)
         if exit_status == 0:
             print(f"File {filename.name!r} compiled successfully ({exit_status}):")
-            if args.verbose:
+            if args.dump and not args.quiet:
                 print(proc.stdout.decode())
         else:
             print(f"File {filename.name!r} failed to compile ({exit_status}):",
@@ -34,10 +34,27 @@ def compile_dump_ir(args: argparse.Namespace):
 
 
 def main():
-    arg_parser = argparse.ArgumentParser()
-    arg_parser.add_argument("-v", "--verbose", action="store_true", help="verbose mode")
+    arg_parser = argparse.ArgumentParser(description=__doc__)
+    subparsers = arg_parser.add_subparsers(title="subcommands", required=True)
+    compile_parser = subparsers.add_parser(
+        "compile", help="compile files to word-oriented IR code"
+    )
+    compile_parser.add_argument(
+        "-d", "--dump", action="store_true",
+        help="dump IR code to stdout"
+    )
+    compile_parser.add_argument(
+        "-q", "--quiet", action="store_true",
+        help="if -d is specified, only emit errors"
+    )
+    compile_parser.add_argument(
+        "-n", "--no-write", action="store_true",
+        help="compile to IR but don't save to BudeBWF file"
+    )
+    compile_parser.set_defaults(handler=compile_handler)
     args = arg_parser.parse_args()
-    compile_dump_ir(args)
+    args.handler(args)
+
 
 if __name__ == "__main__":
     main()
