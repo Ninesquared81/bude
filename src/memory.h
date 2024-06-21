1406b2c7977cf488d5fda344545ae91564884368
Ninesquared81 <96393839+Ninesquared81@users.noreply.github.com>
2024-06-21 01:26:55 +0100

    Introduce helper macros for working with dynamic arrays
    These assume the argument "`da`" is a pointer to a structure
    with the members `count`, `capacity` and `items`. The `items` member
    is a pointer to the array items and should be given the relevant
    type (the macro is type-agnostic but uses `sizeof` so the type must be
    a known in the dynamic array struct itself.

--- src/memory.h
+++ src/memory.h
@@ -13,6 +13,45 @@
         exit(1);                                                 \
         }} while (0)
 
+#define DARRAY_INIT_SIZE 8
+
+#define DARRAY_INIT(da, init_size)                                      \
+    do {                                                                \
+        (da)->capacity = init_size;                                     \
+        (da)->count = 0;                                                \
+        (da)->items = allocate_array(init_size, sizeof (da)->items[0]); \
+        CHECK_ALLOCATION((da)->items);                                  \
+    } while (0)
+
+#define DARRAY_GROW(da)                                                 \
+    do {                                                                \
+        int old_capacity = (da)->capacity;                              \
+        int new_capacity = old_capacity + old_capacity/2;               \
+        if (new_capacity == 0) new_capacity = DARRAY_INIT_SIZE;         \
+        size_t size = sizeof (da)->items[0];                            \
+        void *new_items =                                               \
+            reallocate_array((da)->items, old_capacity * size,          \
+                             new_capacity * size, size);                \
+        CHECK_ALLOCATION(new_items);                                    \
+        (da)->items = new_items;                                        \
+    } while (0)
+
+#define DARRAY_APPEND(da, item)                 \
+    do {                                        \
+        if ((da)->count + 1 > (da)->capacity) { \
+            DARRAY_GROW(da);                    \
+        }                                       \
+        (da)->items[(da)->count++] = item;      \
+    } while (0)
+
+#define DARRAY_FREE(da)                                                 \
+    do {                                                                \
+        free_array((da)->items, (da)->capacity, sizeof (da)->items[0]); \
+        (da)->items = NULL;                                             \
+        (da)->count = 0;                                                \
+        (da)->capacity = 0;                                             \
+    } while (0)
+
 void *allocate_array(size_t count, size_t size);
 void free_array(void *array, [[maybe_unused]] size_t count, [[maybe_unused]] size_t size);
 void *reallocate_array(void *array, size_t old_count, size_t new_count, size_t size);
