f4815ec5936c45c9656f6799ff1986b14748dc23
Ninesquared81 <96393839+Ninesquared81@users.noreply.github.com>
2024-07-07 01:54:49 +0100

    Add main() function to writer.py

--- tool/writer.py
+++ tool/writer.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 """A module for writing Bude Binary Word-oriented Format (BudeBWF) files."""
 
+import argparse
+import sys
 
 import ir
 
@@ -35,3 +37,33 @@ def write_bytecode(module: ir.Module, filename: str,
     write_data(ouput, module, version_number)
     with open(filename, "wb") as f:
         f.write(output)
+
+
+def main() -> None:
+    arg_parser = argparse.ArgumentParser(description="Create a BudeBWF file.")
+    arg_parser.add_argument("filename", help="The file to save into")
+    program = bytearray()
+    while (inp := input("> ")) != "end":
+        if not (inp.startswith("(") and inp.endswith(")")):
+            print("Error, instruction must be encloded in brackets '(', ')'", file=sys.stdout)
+            continue
+        op_string, *operands = inp[1:len(inp)-1].strip().split()
+        try:
+            op = ir.Opcode[op_string]
+        except KeyError:
+            print(f"Invalid opcode {op_string}. Instruction not processed.", file=sys.stderr)
+            continue
+        operand_types = ir.Block.INSTRUCTION_TABLE[op]
+        if (n_real := len(operands)) != (n_types := len(operand_types)):
+            print(f"Incorrect number of operands -- expected {n_types} but got {n_real}.",
+                  "Instruction not processed.",
+                  file=sys.stderr)
+            continue
+        instruction = ir.Instruction(op, *[t(operand) for t, operand in
+                                            zip(operand_types, operands)])
+        program.extend(ir.Block.encode(instruction))
+    print(*ir.Block(program))
+
+
+if __name__ == "__main__":
+    main()
