5500aeb9b7fa710f55522b306c995f836d126495
Ninesquared81 <96393839+Ninesquared81@users.noreply.github.com>
2024-07-18 03:02:48 +0100

    Introduce BudeBWF version 5
    Version 5 introduces the EXTERNAL-FUNCTION-TABLE and
    EXTERNAL-LIBRARY-TABLE sections.

--- src/bwf.c
+++ src/bwf.c
@@ -13,8 +13,10 @@ int get_field_count(int version_number) {
         return 2;
     case 4:
         return 3;
+    case 5:
+        return 5;
     default:
-        static_assert(BWF_version_number <= 4);
+        static_assert(BWF_version_number <= 5);
         assert(0 && "Unreachable");
         return 0;
     }
@@ -27,10 +29,13 @@ int get_function_entry_size(struct function *function, int version_number) {
     case 1:
     case 2:
         return function_code_size;
-    case 3: return 4 + function_code_size;
-    case 4: return 4 + function_code_size + 3*4 + locals_count*4;
+    case 3:
+        return 4 + function_code_size;
+    case 4:
+    case 5:
+        return 4 + function_code_size + 3*4 + locals_count*4;
     default:
-        static_assert(BWF_version_number <= 4);
+        static_assert(BWF_version_number <= 5);
         assert(0 && "Unreachable");
         return 0;
     }
@@ -43,6 +48,7 @@ int get_type_entry_size(const struct type_info *info, int version_number) {
     case 3:
         return 0;
     case 4:
+    case 5:
         switch (info->kind) {
         case KIND_UNINIT:
         case KIND_SIMPLE:
@@ -54,7 +60,39 @@ int get_type_entry_size(const struct type_info *info, int version_number) {
         }
         break;
     default:
-        static_assert(BWF_version_number <= 4);
+        static_assert(BWF_version_number <= 5);
+        assert(0 && "Unreachable");
+    }
+    return 0;
+}
+
+int get_ext_function_entry_size(struct ext_function *external, int version_number) {
+    switch (version_number) {
+    case 1:
+    case 2:
+    case 3:
+    case 4:
+        return 0;
+    case 5:
+        return 2*4 + (external->sig.param_count + external->sig.ret_count)*4 + 2*4;
+    default:
+        static_assert(BWF_version_number <= 5);
+        assert(0 && "Unreachable");
+    }
+    return 0;
+}
+
+int get_ext_library_entry_size(struct ext_library *library, int version_number) {
+    switch (version_number) {
+    case 1:
+    case 2:
+    case 3:
+    case 4:
+        return 0;
+    case 5:
+        return 4 + library->count*4 + 4;
+    default:
+        static_assert(BWF_version_number <= 5);
         assert(0 && "Unreachable");
     }
     return 0;
--- src/bwf.h
+++ src/bwf.h
@@ -1,9 +1,9 @@
 #ifndef BWF_H
 #define BWF_H
 
-#define BWF_version_number 4
+#define BWF_version_number 5
 
-/* Bude Binary Word-oriented Format version 4
+/* Bude Binary Word-oriented Format version 5
  *
  * BudeBWF is a file format for storing word-oriented Bude IR code.
  * The format is structured as a series of fixed-sized fields and variable-sized data entries
@@ -13,7 +13,7 @@
  * The sections are as follows:
  *  - HEADER section comprising the file format's "magic number" (a series of ASCII characters
  *    spelling out "BudeBWF" and the version number (the ASCII character "v" followed by 1 or
- *    more ASCII digits). The current version number for this standard is version 4. The HEADER
+ *    more ASCII digits). The current version number for this standard is version 5. The HEADER
  *    section is terminated by an ASCII line feed character.
  *  - DATA-INFO section holding information pertaining to the data section and -- from version
  *    2 onwards -- the data-info-field-count which holds the number of other fields in this
@@ -29,6 +29,8 @@
  *   string-count:s32
  *   function-count:s32
  *   user-defined-type-count:s32 } version >= 4
+ *   external-function-count:s32 \ version >= 5
+ *   external-library-count:s32  /
  * DATA
  *   STRING-TABLE
  *     size:u32 contents:byte[]
@@ -52,9 +54,31 @@
  *       type-index:s32          |
  *       ...                     |
  *     ...                       /
+ *   EXTERNAL-FUNCTION-TABLE     \
+ *     entry-size:s32            |
+ *     param-count:s32           |
+ *     ret-count:s32             |
+ *     PARAM-LIST                |
+ *       type-index:s32          |
+ *       ...                     |
+ *     RET-LIST                  |
+ *       type-index:s32          |
+ *       ...                     |
+ *     name-index:s32            | version >= 5
+ *     calling-convention:s32    |
+ *     ...                       |
+ *   EXTERNAL-LIBRARY-TABLE      |
+ *     entry-size:s32            |
+ *     external-count:s32        |
+ *     EXTERNAL-LIST             |
+ *       external-index:s32      |
+ *       ...                     |
+ *     filename-index:s32        |
+ *     ...                       /
  *
  */
 
+#include "ext_function.h"
 #include "function.h"
 #include "type.h"
 
@@ -62,5 +86,7 @@
 int get_field_count(int version_number);
 int get_function_entry_size(struct function *function, int version_number);
 int get_type_entry_size(const struct type_info *info, int version_number);
+int get_ext_function_entry_size(struct ext_function *external, int version_number);
+int get_ext_library_entry_size(struct ext_library *library, int version_number);
 
 #endif
