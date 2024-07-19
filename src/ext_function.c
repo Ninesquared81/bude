240241cafea7c777db9f4e1a5cb58e8f78c6a497
Ninesquared81 <96393839+Ninesquared81@users.noreply.github.com>
2024-07-19 03:09:29 +0100

    Add functions to allocate/deallocate external libraries

--- src/ext_function.c
+++ src/ext_function.c
@@ -4,6 +4,7 @@
 #include "memory.h"
 
 #define EXTERNAL_TABLE_INIT_SIZE 64
+#define EXT_LIBRARY_INIT_SIZE 8
 #define EXT_LIB_TABLE_INIT_SIZE 8
 
 void init_external_table(struct external_table *externals) {
@@ -14,11 +15,22 @@ void free_external_table(struct external_table *externals) {
     FREE_DARRAY(externals);
 }
 
+static void init_ext_library(struct ext_library *library) {
+    INIT_DARRAY(library, EXT_LIBRARY_INIT_SIZE);
+}
+
+static void free_ext_library(struct ext_library *library) {
+    FREE_DARRAY(library);
+}
+
 void init_ext_lib_table(struct ext_lib_table *libraries) {
     INIT_DARRAY(libraries, EXT_LIB_TABLE_INIT_SIZE);
 }
 
 void free_ext_lib_table(struct ext_lib_table *libraries) {
+    for (int i = 0; i < libraries->count; ++i) {
+        free_ext_library(&libraries->items[i]);
+    }
     FREE_DARRAY(libraries);
 }
 
@@ -33,4 +45,11 @@ int add_external(struct external_table *externals, struct ext_library *library,
 struct ext_function *get_external(struct external_table *externals, int index) {
     assert(0 <= index && index < externals->count);
     return &externals->items[index];
+}
+
+int add_ext_libarary(struct ext_lib_table *libraries, struct string_view *filename) {
+    DARRAY_APPEND(libraries, (struct ext_library){.filename = *filename});
+    int index = libraries->count - 1;
+    init_ext_library(&libraries->items[index]);
+    return index;
 }
